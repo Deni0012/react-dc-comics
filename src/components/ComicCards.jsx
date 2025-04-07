960ad243620b1fae686a75111dc3b2d153f86fdf
Deni0012 <deni.dg00@icloud.com>
2025-04-07 17:25:12 +0200

    feat: created ComicCards.jsx and imported on Main

---
+++ src/components/ComicCards.jsx
@@ -0,0 +1,18 @@
+// Creazione della funzione ComicCards 
+function ComicCards({ comics }) {
+    return (
+        <>
+            {comics.map((comic) => (
+                <div key={comic.id} className="card-container">
+                    <img src={comic.thumb} alt={comic.title} className="comic-img" />
+                    <h4 className="comic-production">{comic.series}</h4>
+
+                </div>
+            ))}
+
+        </>
+    );
+}
+
+// Esportazione delle comics card
+export default ComicCards;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import comics from "../data/comics"
 import Jumbotron from "./Jumbotron";
-
+import ComicCards from "./ComicCards";
 // Creazione della Funzione Main
 function Main() {
     return (
@@ -8,6 +8,8 @@ function Main() {
             <main>
                 < Jumbotron />
                 <div className="container-main">
+                    <ComicCards comics={comics} />
+                    <div className="current-series">CURRENT SERIES</div>
 
                 </div>
             </main>
